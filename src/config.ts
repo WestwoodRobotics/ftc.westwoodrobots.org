25a0936e52690cc45ae4601c35e8e1ad94b21207
Jihoo <0919imjihoon@gmail.com>
2023-07-10 19:06:40 -0500

    EZ

--- src/config.ts
+++ src/config.ts
@@ -35,7 +35,7 @@ export const SIDEBAR = {
 	en: [
 		
 		{ text: 'Programming', header: true, link: 'programming/' },
-		{ text: 'CAD', header: true, link: 'Setup/' },
+		{ text: 'Setup your coding workspace', header: true, link: 'Setup/' },
 		{ text: 'How to touch grass', link: 'touching-grass'},
 		
 
